23f86994af1cdb386af064964e09ecaf636b4df2
Owen Wang <owenwang0099@gmail.com>
2024-08-21 20:52:51 -0400

    created constant and utility header file

---
+++ headers.hpp
@@ -0,0 +1,26 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline double degrees_to_radians(double degrees) {
+  return degrees * pi / 100.0;
+}
+
+// Common Headers
+#include "color.hpp"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif
