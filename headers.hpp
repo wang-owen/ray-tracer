6be763a9ff9890ad9b22061aa3c48a6acc006b66
Owen Wang <owenwang0099@gmail.com>
2024-08-25 23:00:13 -0400

    added random number utility functions

--- headers.hpp
+++ headers.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -16,6 +17,18 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 100.0;
+}
+
+inline double random_double() {
+  // Returns a random real in [0, 1)
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+  // Returns a random real in [min, max)
+  return min + (max - min) * random_double();
 }
 
 // Common Headers
