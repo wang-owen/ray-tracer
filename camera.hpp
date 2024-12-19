289b6c001b9f3d46eea5168a1c14ece8b98a1a34
Owen Wang <owenwang0099@gmail.com>
2024-12-19 18:27:11 -0500

    added material class and metals

--- camera.hpp
+++ camera.hpp
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.hpp"
+#include "material.hpp"
 
 class camera {
 public:
@@ -95,8 +96,12 @@ private:
 
     hit_record rec;
     if (world.hit(r, interval(0.001, infinity), rec)) {
-      vec3 direction = rec.normal + random_unit_vector();
-      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+      ray scattered;
+      color attenuation;
+      if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+        return attenuation * ray_color(scattered, depth - 1, world);
+      }
+      return color(0, 0, 0);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
--- sphere.hpp
+++ sphere.hpp
@@ -5,8 +5,10 @@
 
 class sphere : public hittable {
 public:
-  sphere(const point3 &center, double radius)
-      : center(center), radius(std::fmax(0, radius)) {}
+  sphere(const point3 &center, double radius, shared_ptr<material> mat)
+      : center(center), radius(std::fmax(0, radius)), mat(mat) {
+    // TODO: Initialize the material pointer `mat`
+  }
 
   bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
     vec3 oc = center - r.origin();
@@ -34,6 +36,7 @@ public:
     rec.p = r.at(rec.t);
     vec3 outward_normal = (rec.p - center) / radius;
     rec.set_face_normal(r, outward_normal);
+    rec.mat = mat;
 
     return true;
   }
@@ -41,6 +44,7 @@ public:
 private:
   point3 center;
   double radius;
+  shared_ptr<material> mat;
 };
 
 #endif
