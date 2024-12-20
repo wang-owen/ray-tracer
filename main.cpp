4e8ff13b4948c0f6b7313bd616bec75dbbca2781
Owen Wang <owenwang0099@gmail.com>
2024-12-19 20:47:30 -0500

    added repositional camera

--- main.cpp
+++ main.cpp
@@ -29,5 +29,10 @@ int main() {
   cam.samples_per_pixel = 100;
   cam.max_depth = 50;
 
+  cam.vfov = 90;
+  cam.lookfrom = point3(-2, 2, 1);
+  cam.lookat = point3(0, 0, -1);
+  cam.vup = vec3(0, 1, 0);
+
   cam.render(world);
 }
